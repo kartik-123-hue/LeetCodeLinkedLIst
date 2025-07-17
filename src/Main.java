16e065266ee3024bec544eedef034d873b1df998
kartik mehta <kartikmehta@kartiks-MacBook-Air.local>
2025-07-17 20:50:31 +0530

    Question 160 chasing pointers solution

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         SolutionForInterSectionOfList solutionForInterSectionOfList=new SolutionForInterSectionOfList();
+        SolutionForInterSectionOfListWithPointer solutionForInterSectionOfListWithPointer=new SolutionForInterSectionOfListWithPointer();
         ListNode headA=new ListNode(4);
         headA.next=new ListNode(1);
         headA.next.next=new ListNode(8);
@@ -16,5 +17,6 @@ public class Main {
         headB.next.next=new ListNode(1);
         headB.next.next.next=headA.next.next;
         System.out.println(solutionForInterSectionOfList.getIntersectionNode(headA,headB).val);
+        System.out.println(solutionForInterSectionOfListWithPointer.getIntersectionNode(headA,headB).val);
     }
 }
